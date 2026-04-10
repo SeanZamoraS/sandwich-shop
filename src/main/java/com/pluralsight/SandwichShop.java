3f9810ce52fd36f9046f1d74d145fcd562329e79
Sean Zamora <zamoraseans@gmail.com>
2026-04-10 14:41:55 -0700

    finished app

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -19,19 +19,49 @@ public class SandwichShop {
 
         int customerAge = 0;
         String sandwichSelection = " ";
+        String loadedSelection = " ";
 
         System.out.println("""
         Hello welcome to the sandwich shop.\s
-        Would you like a small sandwich or a large sandwich? \n""");
+        Would you like a small sandwich or a large sandwich?""");
 
 
         sandwichSelection = input.nextLine();
 
         if (sandwichSelection.equalsIgnoreCase("small")) {
             sandwichTotal = sSandwich;
+
+            System.out.println("\nWould you like that sandwich loaded for $1.00 extra? (y/n): ");
+
+            loadedSelection = input.nextLine();
+
+            if (loadedSelection.equalsIgnoreCase("y")) {
+                sandwichTotal += sLoaded;
+            }
+            else if (loadedSelection.equalsIgnoreCase("n")) {
+                sandwichTotal += 0;
+            }
+            else {
+                System.out.println("\nPlease answer y or n next time.");
+                System.exit(0);
+            }
         }
         else if (sandwichSelection.equalsIgnoreCase("large")) {
             sandwichTotal = lSandwich;
+
+            System.out.println("\nWould you like that sandwich loaded for $1.75 extra? (y/n): ");
+            loadedSelection = input.nextLine();
+
+            if (loadedSelection.equalsIgnoreCase("y")) {
+                sandwichTotal += lLoaded;
+            }
+            else if (loadedSelection.equalsIgnoreCase("n")) {
+                sandwichTotal += 0;
+            }
+            else {
+                System.out.println("\nPlease answer y or n next time.");
+                System.exit(0);
+            }
         }
         else {
             System.out.println("\nPlease make a valid selection next time.");
@@ -42,27 +72,38 @@ public class SandwichShop {
         \nWe have a discount for youth and seniors.
         How old are you?\n""");
 
+        try {
+            customerAge = input.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("Please input a valid age next time.");
+            System.exit(0);
+        }
 
+        sandwichTotal = discount(customerAge, sandwichTotal);
 
+        //test
+        //System.out.println(sandwichTotal);
 
-
-
-
+        System.out.printf("\nAlright, your total is $%.2f.\n", sandwichTotal);
+        System.out.println("Thank you for visiting the sandwich shop");
 
 
     }
 
-    public static double discount(int customerAge, double sandwichTotal) {
+    public static double discount(int age, double total) {
 
-        if (customerAge >= 65) {
-            return sandwichTotal *= 0.8;
+        if (age >= 65) {
+            total *= 0.8;
+            return total;
         }
-        else if (customerAge <= 17) {
-            return sandwichTotal *= 0.9;
+        else if (age <= 17) {
+            total *= 0.9;
+            return total;
         }
         else {
-            System.out.println("\nProceeding with no discounts.");
-            return sandwichTotal;
+            System.out.println("\nNo discounts apply in that case.");
+            return total;
         }
 
     }
